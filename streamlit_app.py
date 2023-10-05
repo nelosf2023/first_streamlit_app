78274dcb0becd3f11db94d90460067d53e87a3ae
nelosf2023 <147084961+nelosf2023@users.noreply.github.com>
2023-10-05 13:16:00 -0600

    Update streamlit_app.py
    re order menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-# add header
+# add header and text
 streamlit.header('Breakfast Menu')
-
-# add text menu
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
