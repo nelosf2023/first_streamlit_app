cd3610580370f50761e4ccbbf73189358bfd5ac8
nelosf2023 <147084961+nelosf2023@users.noreply.github.com>
2023-10-05 13:30:20 -0600

    Update streamlit_app.py
    import pandas
    read in fruit list from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('My Mom\'s New Healthy Diner')
 # add header and text
 streamlit.header('Breakfast Favorites')
@@ -8,3 +10,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 # add another header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# import s3 bucket fruit data
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# show dataframe on page
+streamlit.dataframe(my_fruit_list)
