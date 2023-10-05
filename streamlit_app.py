9adc363d25cd8270c7f27cf1addda0c8806cc730
nelosf2023 <147084961+nelosf2023@users.noreply.github.com>
2023-10-05 13:14:35 -0600

    Update streamlit_app.py
    added menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 # add header
 streamlit.header('Breakfast Menu')
+
+# add text menu
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
