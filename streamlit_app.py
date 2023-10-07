b402b3f940e22241f9d5900d5973a93d8fd2ad7a
nelosf2023 <147084961+nelosf2023@users.noreply.github.com>
2023-10-07 00:04:43 -0600

    Update streamlit_app.py
    remove json version of api response. add kiwi to api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 # add fruity api request
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # change fruity api response from json to dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
