2f1c43e3000daf8ae1a3a8ab001ee1b4ea122ee3
nelosf2023 <147084961+nelosf2023@users.noreply.github.com>
2023-10-10 14:01:30 -0600

    Update streamlit_app.py
    created function and changed error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,24 +29,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-# add fruity api header
-#streamlit.header("Fruityvice Fruit Advice!")
-
-# add user fruit input for api call
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
-
-
-# add fruity api request
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# change fruity api response from json to dataframe
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized) 
-
-
+ 
+#create the repeatable code block (called function)
+def get_fruityvice_data(this_fruit_choice)
+  fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -54,9 +43,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except urllib.error.URLError as e:
    streamlit.error()
  
